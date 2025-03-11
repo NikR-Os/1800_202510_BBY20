1fa2d63ccce0386a1a155eae961dfa395c293105
Nikolas <callmenik2018@outlook.com>
2025-03-11 11:40:51 -0700

    Integrated map box API and added a map view into main.html and main.js

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+
+function showMap() {
+    //------------------------------------------
+    // Defines and initiates basic mapbox data
+    //------------------------------------------
+    // TO MAKE THE MAP APPEAR YOU MUST
+    // ADD YOUR ACCESS TOKEN FROM
+    // https://account.mapbox.com
+    mapboxgl.accessToken = 'pk.eyJ1IjoiLWNsYW5rYXBsdW0tIiwiYSI6ImNtODR0Zm54YzJhenAyanEza2Z3eG50MmwifQ.Kx9Kioj3BBgqC5-pSkZkNg';
+    const map = new mapboxgl.Map({
+        container: 'map', // Container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // Styling URL
+        center: [-122.964274, 49.236082], // Starting position
+        zoom: 8 // Starting zoom
+    });
+
+    // Add user controls to map, zoom bar
+    map.addControl(new mapboxgl.NavigationControl());
+
+    //------------------------------------------------
+    // Add listener for when the map finishes loading.
+    // After loading, we can add map features
+    //------------------------------------------------
+    map.on('load', () => {
+
+        //---------------------------------
+        // Add interactive pins for the hikes
+        //---------------------------------
+        //addHikePins(map);
+
+        //--------------------------------------
+        // Add interactive pin for the user's location
+        //--------------------------------------
+        //addUserPin(map);
+
+    });
+}
+
+showMap();   // Call it! 
+
+
+
